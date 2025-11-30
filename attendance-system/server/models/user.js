b83eb91904f5b30463362e79c64043af4f95030d
mokshith39 <mokshi3953@gmail.com>
2025-11-30 14:36:25 +0530

    changed file name

--- attendance-system/server/models/user.js
+++ attendance-system/server/models/user.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Will be hashed
-  role: { type: String, enum: ['employee', 'manager'], default: 'employee' },
-  employeeId: { type: String, unique: true, required: true }, // e.g., EMP001
-  department: { type: String },
-}, { timestamps: true }); // Automatically adds createdAt
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // Will be hashed
+    role: { type: String, enum: ["employee", "manager"], default: "employee" },
+    employeeId: { type: String, unique: true, required: true }, // e.g., EMP001
+    department: { type: String },
+  },
+  { timestamps: true }
+); // Automatically adds createdAt
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
